20a0d1be0e34b331509199af434b7d8f2e0bd762
quanvo0112 <quanhoconline0112@gmail.com>
2026-05-30 16:08:47 +0700

    Refactor authentication callbacks to simplify token handling and remove unused refresh token logic

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth, { type NextAuthOptions, type Session } from "next-auth";
+import NextAuth, { type NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
@@ -11,43 +11,23 @@ export const authOptions: NextAuthOptions = {
           scope:
             "openid email profile https://www.googleapis.com/auth/calendar.events",
           prompt: "consent",
-              token.accessToken = account.access_token;
-              token.access_token = account.access_token;
           response_type: "code",
         },
       },
-                token.refreshToken = account.refresh_token;
-                token.refresh_token = account.refresh_token;
+    }),
   ],
   callbacks: {
     async jwt({ token, account }) {
       if (account) {
         token.accessToken = account.access_token;
-
-        // Google may only return refresh_token on first consent or when forcing consent.
-        if (account.refresh_token) {
-          token.refreshToken = account.refresh_token;
-              access_token?: string;
-              refresh_token?: string;
-        }
       }
 
       return token;
-            extendedSession.access_token =
-              (token.access_token as string | undefined) ?? extendedSession.accessToken;
-            extendedSession.refresh_token =
-              (token.refresh_token as string | undefined) ?? extendedSession.refreshToken;
     },
     async session({ session, token }) {
-      const extendedSession = session as Session & {
-        accessToken?: string;
-        refreshToken?: string;
-      };
+      session.accessToken = token.accessToken;
 
-      extendedSession.accessToken = token.accessToken as string | undefined;
-      extendedSession.refreshToken = token.refreshToken as string | undefined;
-
-      return extendedSession;
+      return session;
     },
   },
 };
